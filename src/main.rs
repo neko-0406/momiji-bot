b80b53cd46f66e0bc47a9442a91eabc9c47104b7
ryoya <sateraito3696@gmail.com>
2025-11-06 16:09:50 +0900

    オンラインになるように変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{Client, all::{EventHandler, GatewayIntents, Ready, Context}};
+use std::env;
+mod memo;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let token = env::var("DISCORDBOT_TOKEN").expect("トークンの読み込みに失敗しました");
+
+    let intents = GatewayIntents::all();
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(ReadyEvent)
+        .await
+        .expect("クライアントの初期化に失敗しました");
+    
+    if let Err(why) = client.start().await {
+        println!("クライアントの起動に失敗しました: {:?}", why);
+    }
 }
+
+struct ReadyEvent;
+#[serenity::async_trait]
+impl EventHandler for ReadyEvent {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} としてログインしました!", ready.user.name);
+        ctx.online();
+    }
+}
+
